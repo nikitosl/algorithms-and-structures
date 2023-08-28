1c2d284212828b0a86d54733e61010119bf6bbb0
nikita <altuxov.nikita@gmail.com>
2023-08-28 18:57:12 +0400

    Create max_k_elements.py

---
+++ algorithms/max_k_elements.py
@@ -0,0 +1,17 @@
+from structures import MinHeap
+
+def top_k_max(array, k):
+    heap = MinHeap(k)
+    for i, el in enumerate(array):
+        heap.put(el)
+        if i >= k:
+            heap.pop()
+        print(heap.storage)
+
+    result = []
+    for i in range(k):
+        result.append(heap.pop())
+
+    return result
+
+print(top_k_max([8, 11, 2, 3, 4, 5, 6, 7, 3, 10, 1, 2, 3], 6))
